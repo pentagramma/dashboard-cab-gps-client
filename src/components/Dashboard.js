dfafeeccaa79971e525269dbf41b200172e3a916
Sarthak Kumar <famousfive.17@gmail.com>
2024-07-03 20:13:43 +0530

    completed 55%

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,77 +1,80 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
-import { PieChart, Pie, BarChart, Bar, Cell, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import {
+  LineChart,
+  Line,
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts';
+import { GoogleMap, Marker, Polyline, LoadScript } from '@react-google-maps/api';
 
 const Dashboard = () => {
   const [rides, setRides] = useState([]);
   const [selectedMmiId, setSelectedMmiId] = useState('');
+  const [selectedTripId, setSelectedTripId] = useState('');
 
   useEffect(() => {
     const fetchRides = async () => {
-      const response = await axios.get('http://localhost:5000/api/rides');
-      setRides(response.data);
+      try {
+        const response = await axios.get('http://localhost:5000/api/rides');
+        setRides(response.data);
+      } catch (error) {
+        console.error('Error fetching rides data:', error);
+      }
     };
     fetchRides();
   }, []);
 
-  // Data mappings for different charts
-  const fareData = rides.map(ride => ({
-    name: ride.mmi_id,
-    fare: ride.fare,
-  }));
+  const tripsForSelectedMmiId = selectedMmiId ? rides.filter((ride) => ride.mmi_id === selectedMmiId) : rides;
 
-  const distanceData = rides.map(ride => ({
-    name: ride.mmi_id,
-    distance: ride.distance,
-  }));
-
-  const movementDurationData = rides.map(ride => ({
-    name: ride.mmi_id,
-    movementDuration: ride.movement_duration,
-  }));
-
-  const idleDurationData = rides.map(ride => ({
-    name: ride.mmi_id,
-    idleDuration: ride.idle_duration,
-  }));
-
-  const stoppageDurationData = rides.map(ride => ({
-    name: ride.mmi_id,
-    stoppageDuration: ride.stoppage_duration,
-  }));
-
-  const speedData = rides.map(ride => ({
-    name: ride.mmi_id,
-    speed: ride.average_speed,
-  }));
-
-  const startStopLocationData = rides.map(ride => ({
-    name: ride.mmi_id,
-    startLocation: `${ride.drive_locations[0].start_location.lat},${ride.drive_locations[0].start_location.long}`,
-    stopLocation: `${ride.drive_locations[ride.drive_locations.length - 1].end_location.lat},${ride.drive_locations[ride.drive_locations.length - 1].end_location.long}`,
-  }));
-
-  // Handle change in selected MMI ID
-  const handleMmiIdChange = (event) => {
+  const handleMmiIdChange = useCallback((event) => {
     setSelectedMmiId(event.target.value);
-  };
+    setSelectedTripId('');
+  }, []);
 
-  // Filter data based on selected MMI ID
-  const filteredFareData = fareData.filter(data => data.name === selectedMmiId);
-  const filteredDistanceData = distanceData.filter(data => data.name === selectedMmiId);
-  const filteredMovementDurationData = movementDurationData.filter(data => data.name === selectedMmiId);
-  const filteredIdleDurationData = idleDurationData.filter(data => data.name === selectedMmiId);
-  const filteredStoppageDurationData = stoppageDurationData.filter(data => data.name === selectedMmiId);
-  const filteredSpeedData = speedData.filter(data => data.name === selectedMmiId);
-  const filteredStartStopLocationData = startStopLocationData.filter(data => data.name === selectedMmiId);
+  const handleTripIdChange = useCallback((event) => {
+    setSelectedTripId(event.target.value);
+  }, []);
 
-  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+  const aggregateData = useCallback(
+    (key, label) => {
+      return tripsForSelectedMmiId.map((ride, index) => ({ index: index + 1, [label]: ride[key] }));
+    },
+    [tripsForSelectedMmiId]
+  );
+
+  const distanceData = aggregateData('distance', 'km');
+  const movementDurationData = aggregateData('movement_duration', 's');
+  const idleDurationData = aggregateData('idle_duration', 's');
+  const stoppageDurationData = aggregateData('stoppage_duration', 's');
+  const speedData = aggregateData('average_speed', 'km/h');
+
+  const startStopLocationData = tripsForSelectedMmiId.map((ride, index) => ({
+    index: index + 1,
+    startLocation: ride.drive_locations[0]
+      ? `${ride.drive_locations[0].start_location.lat},${ride.drive_locations[0].start_location.long}`
+      : '',
+    stopLocation:
+      ride.drive_locations.length > 0
+        ? `${ride.drive_locations[ride.drive_locations.length - 1].end_location.lat},${ride.drive_locations[ride.drive_locations.length - 1].end_location.long}`
+        : '',
+  }));
+
+  const xAxisTicks = Array.from({ length: 8 }, (_, i) => (i + 1) * 5);
 
   return (
     <div className="grid grid-cols-3 gap-4 p-4">
       {/* Dropdown for MMI IDs */}
       <div className="col-span-3 mb-4">
-        <label htmlFor="mmiId" className="font-semibold">Select MMI ID:</label>
+        <label htmlFor="mmiId" className="font-semibold">
+          Select MMI ID:
+        </label>
         <select
           id="mmiId"
           className="ml-2 p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500"
@@ -79,36 +82,12 @@ const Dashboard = () => {
           onChange={handleMmiIdChange}
         >
           <option value="">All</option>
-          {rides.map(ride => (
-            <option key={ride.mmi_id} value={ride.mmi_id}>{ride.mmi_id}</option>
+          {rides.map((ride) => (
+            <option key={ride._id} value={ride.mmi_id}>
+              {ride.mmi_id}
+            </option>
           ))}
         </select>
-      </div>
-
-      {/* Fare Chart */}
-      <div className="bg-white p-4 rounded-lg shadow">
-        <h2 className="text-lg font-semibold mb-4">Fare</h2>
-        <div className="h-96">
-          <ResponsiveContainer width="100%" height="100%">
-            <PieChart>
-              <Pie
-                data={filteredFareData}
-                cx="50%"
-                cy="50%"
-                labelLine={false}
-                label={({ name }) => name}
-                outerRadius={80}
-                fill="#8884d8"
-                dataKey="fare"
-              >
-                {filteredFareData.map((entry, index) => (
-                  <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                ))}
-              </Pie>
-              <Tooltip />
-            </PieChart>
-          </ResponsiveContainer>
-        </div>
       </div>
 
       {/* Distance Chart */}
@@ -116,13 +95,13 @@ const Dashboard = () => {
         <h2 className="text-lg font-semibold mb-4">Distance</h2>
         <div className="h-96">
           <ResponsiveContainer width="100%" height="100%">
-            <LineChart data={filteredDistanceData}>
+            <LineChart data={distanceData}>
               <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
+              <XAxis dataKey="index" ticks={xAxisTicks} />
               <YAxis />
               <Tooltip />
               <Legend />
-              <Line type="monotone" dataKey="distance" stroke="#8884d8" />
+              <Line type="monotone" dataKey="km" stroke="#8884d2" />
             </LineChart>
           </ResponsiveContainer>
         </div>
@@ -133,13 +112,13 @@ const Dashboard = () => {
         <h2 className="text-lg font-semibold mb-4">Movement Duration</h2>
         <div className="h-96">
           <ResponsiveContainer width="100%" height="100%">
-            <BarChart data={filteredMovementDurationData}>
+            <BarChart data={movementDurationData}>
               <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
+              <XAxis dataKey="index" ticks={xAxisTicks} />
               <YAxis />
               <Tooltip />
               <Legend />
-              <Bar dataKey="movementDuration" fill="#8884d8" />
+              <Bar dataKey="s" fill="#FACA15" />
             </BarChart>
           </ResponsiveContainer>
         </div>
@@ -150,13 +129,13 @@ const Dashboard = () => {
         <h2 className="text-lg font-semibold mb-4">Idle Duration</h2>
         <div className="h-96">
           <ResponsiveContainer width="100%" height="100%">
-            <BarChart data={filteredIdleDurationData}>
+            <BarChart data={idleDurationData}>
               <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
+              <XAxis dataKey="index" ticks={xAxisTicks} />
               <YAxis />
               <Tooltip />
               <Legend />
-              <Bar dataKey="idleDuration" fill="#8884d8" />
+              <Bar dataKey="s" fill="#31C48D" />
             </BarChart>
           </ResponsiveContainer>
         </div>
@@ -167,13 +146,13 @@ const Dashboard = () => {
         <h2 className="text-lg font-semibold mb-4">Stoppage Duration</h2>
         <div className="h-96">
           <ResponsiveContainer width="100%" height="100%">
-            <BarChart data={filteredStoppageDurationData}>
+            <BarChart data={stoppageDurationData}>
               <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
+              <XAxis dataKey="index" ticks={xAxisTicks} />
               <YAxis />
               <Tooltip />
               <Legend />
-              <Bar dataKey="stoppageDuration" fill="#8884d8" />
+              <Bar dataKey="s" fill="#1C64F2" />
             </BarChart>
           </ResponsiveContainer>
         </div>
@@ -184,13 +163,13 @@ const Dashboard = () => {
         <h2 className="text-lg font-semibold mb-4">Speed</h2>
         <div className="h-96">
           <ResponsiveContainer width="100%" height="100%">
-            <LineChart data={filteredSpeedData}>
+            <LineChart data={speedData}>
               <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
+              <XAxis dataKey="index" ticks={xAxisTicks} />
               <YAxis />
               <Tooltip />
               <Legend />
-              <Line type="monotone" dataKey="speed" stroke="#8884d8" />
+              <Line type="monotone" dataKey="km/h" stroke="#E02424" />
             </LineChart>
           </ResponsiveContainer>
         </div>
@@ -200,17 +179,49 @@ const Dashboard = () => {
       <div className="bg-white p-4 rounded-lg shadow">
         <h2 className="text-lg font-semibold mb-4">Start/Stop Location</h2>
         <div className="h-96">
-          <ResponsiveContainer width="100%" height="100%">
-            <LineChart data={filteredStartStopLocationData}>
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Line type="monotone" dataKey="startLocation" stroke="#8884d8" />
-              <Line type="monotone" dataKey="stopLocation" stroke="#82ca9d" />
-            </LineChart>
-          </ResponsiveContainer>
+          <LoadScript googleMapsApiKey="AIzaSyCyaFfzx2egZfBNTFFXX3HRP-ypSBQhd28">
+            <GoogleMap
+              mapContainerStyle={{ width: '100%', height: '100%' }}
+              zoom={10}
+              center={{ lat: 0, lng: 0 }}
+            >
+              {startStopLocationData.map((data, index) => (
+                <React.Fragment key={index}>
+                  {data.startLocation && (
+                    <Marker
+                      position={{
+                        lat: parseFloat(data.startLocation.split(',')[0]),
+                        lng: parseFloat(data.startLocation.split(',')[1]),
+                      }}
+                    />
+                  )}
+                  {data.stopLocation && (
+                    <Marker
+                      position={{
+                        lat: parseFloat(data.stopLocation.split(',')[0]),
+                        lng: parseFloat(data.stopLocation.split(',')[1]),
+                      }}
+                    />
+                  )}
+                  {data.startLocation && data.stopLocation && (
+                    <Polyline
+                      path={[
+                        {
+                          lat: parseFloat(data.startLocation.split(',')[0]),
+                          lng: parseFloat(data.startLocation.split(',')[1]),
+                        },
+                        {
+                          lat: parseFloat(data.stopLocation.split(',')[0]),
+                          lng: parseFloat(data.stopLocation.split(',')[1]),
+                        },
+                      ]}
+                      options={{ strokeColor: '#FF0000', strokeWeight: 2 }}
+                    />
+                  )}
+                </React.Fragment>
+              ))}
+            </GoogleMap>
+          </LoadScript>
         </div>
       </div>
     </div>
