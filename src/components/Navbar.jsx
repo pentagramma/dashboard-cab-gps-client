a929647cac015099b5b01e7841ea2fb7f6b08002
Sarthak Kumar <famousfive.17@gmail.com>
2024-07-07 17:36:12 +0530

    s

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -57,23 +57,8 @@ const Navbar = forwardRef(({ selectedMmiId, handleMmiIdChange, rides }, ref) => 
   return (
     <div ref={ref} className='w-full h-[70px] bg-purple-950 text-white flex items-center justify-between px-10 font-ruda'>
       <div>
-        <label htmlFor="mmiId" className="font-semibold mr-2 text-yellow-400">Select MMI ID:</label>
-        <select
-          id="mmiId"
-          className="p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500 text-black"
-          value={selectedMmiId}
-          onChange={(e) => {
-            handleMmiIdChange(e);
-          }}
-          disabled={isDetailedDashboard} // Disable the dropdown if on the detailed dashboard
-        >
-          <option value="">All</option>
-          {rides.map((ride) => (
-            <option key={ride._id} value={ride.mmi_id}>
-              {ride.mmi_id}
-            </option>
-          ))}
-        </select>
+        <h1>Start Time: </h1>
+        <h1>End Time: </h1>
       </div>
       <div className="text-center flex-grow">
         {selectedMmiId && (
