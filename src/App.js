8b228c63454bb3e6664a25dc11056032da94b895
Sarthak Kumar <famousfive.17@gmail.com>
2024-07-08 11:40:32 +0530

    s

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
     const fetchRides = async () => {
       setLoading(true); // Set loading to true when fetching starts
       try {
-        const response = await axios.get("http://localhost:5000/api/rides");
+        const response = await axios.get("http://localhost:5001/api/rides");
         setRides(response.data);
         setLoading(false); // Set loading to false when fetching ends
       } catch (error) {
